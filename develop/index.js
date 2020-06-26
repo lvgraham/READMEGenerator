c0c4e5d97f034f5273535431fa428ada37f6982e
lvgraham <liavibanez@gmail.com>
2020-06-25 21:20:50 -0500

    adding qustions obj

--- develop/index.js
+++ develop/index.js
@@ -1,5 +1,61 @@
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Enter a description.",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Write a table of contents (separated by commas)",
+        name: "tableOfContents"
+    },
+    {
+        type: "input",
+        message: "What are the installation guidelines",
+        name: "install"
+    },
+    {
+        type: "input",
+        message: "What is your application used for?",
+        name: "usage"
+    },
+    {
+        type: "checkbox",
+        message: "Select a license",
+        name: "license"
+        choices: [
+            'Apache License 2.0', 
+            'GNU General Public License v3.0', 
+            'MIT license', 
+            'Mozzila Public License 2.0',
+        ]
+    },
+    {
+        type: "input",
+        message: "Who is(are) the author(s)?",
+        name: "author"
+    },
+    {
+        type: "input",
+        message: "What are the rules for contributing?",
+        name: "contribute"
+    },
+    {
+        type: "input",
+        message: "Tests",
+        name: "test"
+    },
+    {
+        type: "input",
+        message: "enter contact information for any questions. ",
+        name: "questions"
+    }
 
 ];
 
