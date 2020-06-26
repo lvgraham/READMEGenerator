7f5a9b23d4cf583ccd57e475f9ee0188c7e97a16
lvgraham <liavibanez@gmail.com>
2020-06-25 22:00:04 -0500

    adding title + description to readme file

--- develop/index.js
+++ develop/index.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const { error } = require('console');
 
 // array of questions for user
 const questions = [
@@ -12,68 +14,100 @@ const questions = [
         message: "Enter a description.",
         name: "description"
     },
-    {
-        type: "input",
-        message: "Write a table of contents (separated by commas)",
-        name: "tableOfContents"
-    },
-    {
-        type: "input",
-        message: "What are the installation guidelines",
-        name: "install"
-    },
-    {
-        type: "input",
-        message: "What is your application used for?",
-        name: "usage"
-    },
-    {
-        type: "checkbox",
-        message: "Select a license",
-        name: "license",
-        choices: [
-            'Apache License 2.0', 
-            'GNU General Public License v3.0', 
-            'MIT license', 
-            'Mozzila Public License 2.0',
-        ]
-    },
-    {
-        type: "input",
-        message: "Who is(are) the author(s)?",
-        name: "author"
-    },
-    {
-        type: "input",
-        message: "What are the rules for contributing?",
-        name: "contribute"
-    },
-    {
-        type: "input",
-        message: "Tests",
-        name: "test"
-    },
-    {
-        type: "input",
-        message: "enter contact information for any questions. ",
-        name: "questions"
-    }
+    // {
+    //     type: "input",
+    //     message: "Write a table of contents (separated by commas)",
+    //     name: "tableOfContents"
+    // },
+    // {
+    //     type: "input",
+    //     message: "What are the installation guidelines",
+    //     name: "install"
+    // },
+    // {
+    //     type: "input",
+    //     message: "What is your application used for?",
+    //     name: "usage"
+    // },
+    // {
+    //     type: "checkbox",
+    //     message: "Select a license",
+    //     name: "license",
+    //     choices: [
+    //         'Apache License 2.0', 
+    //         'GNU General Public License v3.0', 
+    //         'MIT license', 
+    //         'Mozzila Public License 2.0',
+    //     ]
+    // },
+    // {
+    //     type: "input",
+    //     message: "Who is(are) the author(s)?",
+    //     name: "author"
+    // },
+    // {
+    //     type: "input",
+    //     message: "What are the rules for contributing?",
+    //     name: "contribute"
+    // },
+    // {
+    //     type: "input",
+    //     message: "Tests",
+    //     name: "test"
+    // },
+    // {
+    //     type: "input",
+    //     message: "enter contact information for any questions. ",
+    //     name: "questions"
+    // }
 
 ];
 
 inquirer 
     .prompt(questions).then(response => {
+        //creating readme file
+        const filename = 'README.md'
+        // function to add title write README file
+        fs.appendFileSync(filename, '# ' + response.title + '\n', function(err){
+            if (err) {
+                return console.log(err);
+            }
+            console.log('Success!');
+        });
 
-        const filename = data.name.toLowerCase().split(' ').join('') + '.json';
-        // function to write README file
-        function writeToFile(fileName, data) {
-            
-        }
+        //function to add description to README.md 
+        fs.appendFileSync(filename, '## Description \n' + response.description + '\n', function(err){
+            if (err) {
+                return console.log(err);
+            }
+            console.log('Success!');
+        });
 
-        // function to initialize program
-        function init() {
+        // fs.appendFileSync(filename, '#' + response.title + '\n', function(err){
+        //     if (err) {
+        //         return console.log(err);
+        //     }
+        //     console.log('Success!');
+        // });
 
-        }
+        // fs.appendFileSync(filename, '#' + response.title + '\n', function(err){
+        //     if (err) {
+        //         return console.log(err);
+        //     }
+        //     console.log('Success!');
+        // });
+
+        // fs.appendFileSync(filename, '#' + response.title + '\n', function(err){
+        //     if (err) {
+        //         return console.log(err);
+        //     }
+        //     console.log('Success!');
+        // });
+
+        // // function to initialize program
+        // function init() {
+
+        // }
     
 
 
@@ -82,4 +116,4 @@ inquirer
     })
 
 // function call to initialize program
-init();
+// init();
